248fd6d2a555bc853e356e4d702e30af3a83bd6e
Steven <stevensvg59@gmail.com>
2024-06-15 16:13:00 -0500

    Added sort for name and date & search by customer name

--- MegaDesk3.0/Pages/Desks/Index.cshtml.cs
+++ MegaDesk3.0/Pages/Desks/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MegaDesk3._0.Data;
 using MegaDesk3._0.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace MegaDesk3._0.Pages.Desks
 {
@@ -21,9 +23,54 @@ namespace MegaDesk3._0.Pages.Desks
 
         public IList<Desk> Desk { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        //Adding search for customer name
+        [BindProperty(SupportsGet = true)]
+        public string? SearchName { get; set; }
+
+        //For sorting
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentSort { get; set; }
+        
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            Desk = await _context.Desk.ToListAsync();
+            // Adding sorting
+            CurrentSort = sortOrder;
+            NameSort = sortOrder == "name" ? "name_desc" : "name";
+            DateSort = sortOrder == "date" ? "date_desc" : "date";
+            // 
+
+
+            var desks = from d in _context.Desk
+                         select d;
+
+            // Adding sorting
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    desks = desks.OrderByDescending(d => d.Name);
+                    break;
+                case "name":
+                    desks = desks.OrderBy(d => d.Name);
+                    break;
+                case "date_desc":
+                    desks = desks.OrderByDescending(d => d.Date);
+                    break;
+                case "date":
+                default:
+                    desks = desks.OrderBy(d => d.Date);
+                    break;
+            }
+            //
+
+
+            if (!string.IsNullOrEmpty(SearchName))
+            {
+                desks = desks.Where(d => d.Name.Contains(SearchName));
+            }
+
+            Desk = await desks.ToListAsync();
         }
     }
 }
