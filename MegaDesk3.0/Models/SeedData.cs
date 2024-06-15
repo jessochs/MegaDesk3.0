1bb82010d53aff6e75fd18912ff4d80be4a54ef2
DiegoArrC <castanedadiego12.dac@gmail.com>
2024-06-15 13:47:04 -0700

    Added seed data

---
+++ MegaDesk3.0/Models/SeedData.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MegaDesk3._0.Data;
+
+
+namespace MegaDesk3._0.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MegaDesk3_0Context(
+                serviceProvider.GetRequiredService<DbContextOptions<MegaDesk3_0Context>>()))
+            {
+                if (context == null || context.Desk == null)
+                {
+                    throw new ArgumentNullException("Null MegaDesk3_0Context");
+                }
+
+                if (context.Desk.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Desk.AddRange(
+                    new Desk
+                    {
+                        Name = "John Doe",
+                        Date = DateTime.Parse("2024-03-14"),
+                        Width = 24,
+                        Height = 24,
+                        Drawers = 2,
+                        Material = "Oak",
+                        Rush = 3,
+                        Price = 460 //50 * 2 + 200 + 200
+                    },
+                                       
+                    new Desk
+                    {
+                        Name = "Jane Dawson",
+                        Date = DateTime.Parse("2024-06-07"),
+                        Width = 36,
+                        Height = 24,
+                        Drawers = 2,
+                        Material = "Laminate",
+                        Rush = 5,
+                        Price = 440 //200 base + 0 surface + 50 * 2 drawers + 100 material + 40 rush
+                    },
+                                                          
+                    new Desk
+                    {
+                        Name = "John Smith",
+                        Date = DateTime.Parse("2023-12-01"),
+                        Width = 48,
+                        Height = 24,
+                        Drawers = 2,
+                        Material = "Pine",
+                        Rush = 7,
+                        Price = 537 //200 base + (48*24) % 1000 = 152 surface area + 50*2 drawers + 50 material + 35 rush 
+                    },
+                                                                             
+                    new Desk
+                    {
+                        Name = "Jane Johanson",
+                        Date = DateTime.Parse("2024-06-14"),
+                        Width = 60,
+                        Height = 24,
+                        Drawers = 3,
+                        Material = "Rosewood",
+                        Rush = 14,
+                        Price = 1090 // 200 base + 60*24 % 1000 = 440 surface area + 50*3 drawers + 300 material + 0 rush
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MegaDesk3.0/Program.cs
+++ MegaDesk3.0/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MegaDesk3._0.Data;
+using MegaDesk3._0.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +10,24 @@ builder.Services.AddDbContext<MegaDesk3_0Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MegaDesk3_0Context") ?? throw new InvalidOperationException("Connection string 'MegaDesk3_0Context' not found.")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<MegaDesk3_0Context>();
+        context.Database.EnsureCreated();
+        SeedData.Initialize(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
