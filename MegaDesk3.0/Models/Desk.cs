55e6e3f74eb15891c55e59dd1f92de1f485a5264
jessochs <fac17002@byui.edu>
2024-06-15 16:25:37 -0700

    validation

--- MegaDesk3.0/Models/Desk.cs
+++ MegaDesk3.0/Models/Desk.cs
@@ -5,11 +5,22 @@ namespace MegaDesk3._0.Models
     public class Desk
     {
         public int Id { get; set; }
+        [StringLength(40, MinimumLength = 4)]
+        [Required]
         public string? Name { get; set; } 
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+        
+        [Range(24, 96)]
+        [Required]
         public int Width { get; set; }
+       
+        [Range(12, 48)]
+        [Required]
         public int Height { get; set; }
+
+        [Range(0, 7)]
+        [Required]
         public int Drawers {  get; set; }
         public string? Material { get; set; }
         public int Rush {  get; set; }
